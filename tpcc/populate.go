c4dce27df5e36c18a2c843b5db3813fea36e721e
Andrew Couch <hi@andrewcou.ch>
2017-08-05 14:03:39 -0400

    wip: populate done?

--- tpcc/populate.go
+++ tpcc/populate.go
@@ -7,11 +7,14 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"time"
 
 	"github.com/cockroachdb/cockroach-go/crdb"
 )
 
 const BATCH_SIZE = 500
+
+var loadTime string = time.Now().Format("'2006-01-02T15:04:05'")
 
 // Performs a one-indexed group by
 func groupBy(group_size, count int) (item, group int) {
@@ -37,6 +40,7 @@ func populateTable(
 	rand *Rand,
 	table string,
 	cardinality int,
+	batch int,
 	maker func(rand *Rand, count int) string,
 ) error {
 	if *verbose {
@@ -44,10 +48,10 @@ func populateTable(
 	}
 
 	prefix := fmt.Sprintf("INSERT INTO %s VALUES ", table)
-	rows := make([]string, 0, BATCH_SIZE)
+	rows := make([]string, 0, batch)
 
 	for count, i := 0, 0; count < cardinality; i++ {
-		if i == BATCH_SIZE {
+		if i == batch {
 			err := insertRows(db, prefix, rows)
 			if err != nil {
 				return err
@@ -152,7 +156,7 @@ func makeDistrict(rand *Rand, count int) string {
 
 const CUSTOMERS_PER_DISTRICT int = 30 //00
 const CUSTOMERS_PER_WAREHOUSE int = CUSTOMERS_PER_DISTRICT * DISTRICTS_PER_WAREHOUSE
-const CUSTOMERS_PLACES string = "(%v,%v,%v,'%s','%s','%s','%s','%s','%s','%s','%s','%s',DEFAULT,'%s',%0.2f,%0.4f,%0.2f,%0.2f,%v,%v,'%s')"
+const CUSTOMERS_PLACES string = "(%v,%v,%v,'%s','%s','%s','%s','%s','%s','%s','%s','%s',%s,'%s',%0.2f,%0.4f,%0.2f,%0.2f,%v,%v,'%s')"
 
 func makeCustomer(rand *Rand, count int) string {
 	c_id, district := groupBy(CUSTOMERS_PER_DISTRICT, count)
@@ -173,6 +177,7 @@ func makeCustomer(rand *Rand, count int) string {
 	c_state := rand.RandAString(2, 2)
 	c_zip := rand.RandZip()
 	c_phone := rand.RandNString(16, 16)
+	c_since := loadTime
 
 	c_credit := "GC"
 	if rand.Rand(1, 10) == 1 {
@@ -190,30 +195,46 @@ func makeCustomer(rand *Rand, count int) string {
 	return fmt.Sprintf(
 		CUSTOMERS_PLACES, c_id, c_d_id, c_w_id, c_last, c_middle, c_first,
 		c_street_1, c_street_2, c_city, c_state, c_zip, c_phone,
-		c_credit, c_credit_lim, c_discount, c_balance,
+		c_since, c_credit, c_credit_lim, c_discount, c_balance,
 		c_ytd_payment, c_payment_cnt, c_delivery_cnt, c_data,
 	)
 }
 
-const HISTORY_PLACES string = "(%v,%v,%v,%v,%v,DEFAULT,%0.2f,'%s')"
+const HISTORY_PLACES string = "(%v,%v,%v,%v,%v,%s,%0.2f,'%s')"
 
 func makeHistory(rand *Rand, count int) string {
 	h_c_id, district := groupBy(CUSTOMERS_PER_DISTRICT, count)
 	h_c_d_id, h_c_w_id := groupBy(DISTRICTS_PER_WAREHOUSE, district)
 	h_d_id, h_w_id := h_c_d_id, h_c_w_id
+	h_date := loadTime
 	h_amount := 10.0
 	h_data := rand.RandAString(12, 24)
 
 	return fmt.Sprintf(
-		HISTORY_PLACES, h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_amount, h_data,
+		HISTORY_PLACES, h_c_id, h_c_d_id, h_c_w_id, h_d_id, h_w_id, h_date, h_amount, h_data,
 	)
 }
 
 var o_c_ids []int
+var o_ol_cnts = make(map[int]map[int]map[int]int) // w_id - d_id - o_id - cnt
+
+func putCount(w_id, d_id, o_id int, cnt int64) {
+	w_cnts, ok := o_ol_cnts[w_id]
+	if !ok {
+		w_cnts = make(map[int]map[int]int)
+		o_ol_cnts[w_id] = w_cnts
+	}
+	d_cnts, ok := w_cnts[d_id]
+	if !ok {
+		d_cnts = make(map[int]int)
+		w_cnts[d_id] = d_cnts
+	}
+	d_cnts[o_id] = int(cnt)
+}
 
 const ORDERS_PER_DISTRICT int = CUSTOMERS_PER_DISTRICT
 const ORDERS_PER_WAREHOUSE int = ORDERS_PER_DISTRICT * DISTRICTS_PER_WAREHOUSE
-const ORDERS_PLACES string = "(%v,%v,%v,%v,DEFAULT,%v,%v,%v)"
+const ORDERS_PLACES string = "(%v,%v,%v,%v,%s,%v,%v,%v)"
 
 func makeOrder(rand *Rand, count int) string {
 	o_id, district := groupBy(ORDERS_PER_DISTRICT, count)
@@ -225,32 +246,78 @@ func makeOrder(rand *Rand, count int) string {
 	o_c_id := o_c_ids[0]
 	o_c_ids = o_c_ids[1:]
 
+	o_entry_d := loadTime
+
 	o_carrier_id := "NULL"
-	if o_id < 2101 {
+	if o_id <= NUM_ORDERS_BEFORE_NEWORDER {
 		o_carrier_id = fmt.Sprintf("%v", rand.Rand(1, 10))
 	}
 
 	o_ol_cnt := rand.Rand(5, 15)
+	putCount(o_w_id, o_d_id, o_id, o_ol_cnt)
+
 	o_all_local := 1
 
 	return fmt.Sprintf(
 		ORDERS_PLACES, o_id, o_d_id, o_w_id, o_c_id,
-		o_carrier_id, o_ol_cnt, o_all_local,
+		o_entry_d, o_carrier_id, o_ol_cnt, o_all_local,
 	)
 }
 
 const NEWORDERS_PER_DISTRICT int = 9 //00
 const NEWORDERS_PER_WAREHOUSE int = NEWORDERS_PER_DISTRICT * DISTRICTS_PER_WAREHOUSE
 const NEWORDERS_PLACES string = "(%v,%v,%v)"
-const FIRST_NEWORDER_ID int = ORDERS_PER_DISTRICT - NEWORDERS_PER_DISTRICT
+const NUM_ORDERS_BEFORE_NEWORDER int = ORDERS_PER_DISTRICT - NEWORDERS_PER_DISTRICT
 
 func makeNewOrder(rand *Rand, count int) string {
 	order, district := groupBy(NEWORDERS_PER_DISTRICT, count)
-	no_o_id := order + FIRST_NEWORDER_ID
+	no_o_id := order + NUM_ORDERS_BEFORE_NEWORDER
 	no_d_id, no_w_id := groupBy(DISTRICTS_PER_WAREHOUSE, district)
 
 	return fmt.Sprintf(
 		NEWORDERS_PLACES, no_o_id, no_d_id, no_w_id,
+	)
+}
+
+func makeOrderLines(rand *Rand, count int) string {
+	ol_o_id, district := groupBy(ORDERS_PER_DISTRICT, count)
+	ol_d_id, ol_w_id := groupBy(DISTRICTS_PER_WAREHOUSE, district)
+
+	cnt := o_ol_cnts[ol_w_id][ol_d_id][ol_o_id]
+	values := make([]string, cnt)
+
+	for i := 0; i < cnt; i++ {
+		values[i] = makeOrderLine(rand, i, ol_o_id, ol_d_id, ol_w_id)
+	}
+
+	return strings.Join(values, ",")
+}
+
+const ORDER_LINE_PLACES string = "(%v,%v,%v,%v,%v,%v,%s,%v,%0.2f,'%s')"
+
+func makeOrderLine(rand *Rand, ol_number, ol_o_id, ol_d_id, ol_w_id int) string {
+	ol_i_id := rand.Rand(1, ITEMS_COUNT)
+	ol_supply_w_id := ol_w_id
+
+	ol_delivery_d := "NULL"
+	if ol_o_id <= NUM_ORDERS_BEFORE_NEWORDER {
+		ol_delivery_d = loadTime
+	}
+
+	ol_quantity := 5
+
+	ol_amount := 0.0
+	if ol_o_id <= NUM_ORDERS_BEFORE_NEWORDER {
+		ol_amount = float64(rand.Rand(1, 999999)) / 1000000.0
+	}
+
+	ol_dist_info := rand.RandAString(24, 24)
+
+	return fmt.Sprintf(
+		ORDER_LINE_PLACES,
+		ol_o_id, ol_d_id, ol_w_id, ol_number,
+		ol_i_id, ol_supply_w_id, ol_delivery_d,
+		ol_quantity, ol_amount, ol_dist_info,
 	)
 }
 
@@ -259,20 +326,22 @@ func Populate(db *sql.DB, rand *Rand, W int) error {
 		name  string
 		card  int
 		maker func(rand *Rand, count int) string
+		batch int
 	}{
-		{"item", ITEMS_COUNT, makeItem},
-		{"warehouse", W, makeWarehouse},
-		{"stock", W * STOCK_PER_WAREHOUSE, makeStock},
-		{"district", W * DISTRICTS_PER_WAREHOUSE, makeDistrict},
-		{"customer", W * CUSTOMERS_PER_WAREHOUSE, makeCustomer},
-		{"history", W * CUSTOMERS_PER_WAREHOUSE, makeHistory},
-		{"\"order\"", W * ORDERS_PER_WAREHOUSE, makeOrder},
-		{"newOrder", W * NEWORDERS_PER_WAREHOUSE, makeNewOrder},
+		{"item", ITEMS_COUNT, makeItem, BATCH_SIZE},
+		{"warehouse", W, makeWarehouse, BATCH_SIZE},
+		{"stock", W * STOCK_PER_WAREHOUSE, makeStock, BATCH_SIZE},
+		{"district", W * DISTRICTS_PER_WAREHOUSE, makeDistrict, BATCH_SIZE},
+		{"customer", W * CUSTOMERS_PER_WAREHOUSE, makeCustomer, BATCH_SIZE},
+		{"history", W * CUSTOMERS_PER_WAREHOUSE, makeHistory, BATCH_SIZE},
+		{"\"order\"", W * ORDERS_PER_WAREHOUSE, makeOrder, BATCH_SIZE},
+		{"newOrder", W * NEWORDERS_PER_WAREHOUSE, makeNewOrder, BATCH_SIZE},
+		{"orderLine", W * ORDERS_PER_WAREHOUSE, makeOrderLines, BATCH_SIZE / 10},
 	}
 
 	var err error
 	for _, t := range table_data {
-		err = populateTable(db, rand, t.name, t.card, t.maker)
+		err = populateTable(db, rand, t.name, t.card, t.batch, t.maker)
 		if err != nil {
 			return err
 		}
